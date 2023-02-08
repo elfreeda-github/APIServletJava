5c6ac004fe54b38f5755e74cd4b95e7148f34da7
elfreeda <freedadcunha@gmail.com>
2023-02-08 12:56:30 +0530

    Update CityDAO.java
    Removing unnecessary comments

--- JavaAPIServletProgram/src/com/he/api/CityDAO.java
+++ JavaAPIServletProgram/src/com/he/api/CityDAO.java
@@ -23,10 +23,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-/**
- *
- * @author divyansh
- */
 class CityDAO {
 
 	private static final String DATA_FILE_PATH = "src/com/he/api/data.xml";
